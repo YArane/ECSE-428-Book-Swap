5193dc051d05d705040033f6b445ad0dac3180e6
yarane <yarden.arane@gmail.com>
2016-01-27 20:28:19 -0500

    password requirements unit test

--- account_management/create_account.py
+++ account_management/create_account.py
@@ -1,13 +1,15 @@
 import re
 
-rgx_password_requirements = [r'(.a*[a-z])', \
+rgx_password_requirements = [r'(.*[a-z])', \
                          r'(.*[A-Z])', \
                          r'(.*[0-9])', \
-                         r'(.[a-zA-Z0-9]{8,30})']
+                         r'(^\w{8,}$)', \
+                         r'(^\w{,30}$)']
 password_requirements = ['lowercase required', \
                          'uppercase required', \
                          'number required', \
-                         'min len = 8, max len = 30']
+                         'min length = 8', \
+                         'max length = 30']
 
 """ Validates user input password using regex
 to find rules that are not obeyed
@@ -16,6 +18,15 @@ to find rules that are not obeyed
     return: list of rules which failed
 """
 def validate_password_requirement(password):
+    # check for string type
+    if not password:
+        return ['field is required']
+    if not isinstance(password, basestring):
+        return ['alphanumeric characters only']
+    if not password.isalnum():
+        return ['alphanumeric characters only']
+
+    # check regular expressions
     failed_rules = []
     for rgx, requirement in zip(rgx_password_requirements, password_requirements):
         result = re.match(rgx, password)
@@ -24,6 +35,3 @@ def validate_password_requirement(password):
 
     return failed_rules
 
-password = 'YSNAPREBADA'
-
-print validate_password_requirement(password)
---
+++ unit_tests/README.md
@@ -0,0 +1,43 @@
+#Unit Tests#
+
+Unit tests will be conducted using the [unittest](https|//docs.python.org/2/library/unittest.html#module-unittest) framework provided in python 2.1
+
+##Test Case Reference##
+Method | Checks That
+------ | -----------
+assertEqual(a, b)         | a == b
+assertNotEqual(a, b)      | a != b
+assertTrue(x)             | bool(x) is True
+assertFalse(x)            | bool(x) is False
+assertIs(a, b)            | a is b
+assertIsNot(a, b)         | a is not b
+assertIsNone(x)           | x is None
+assertIsNotNone(x)        | x is not None
+assertIn(a, b)            | a in b
+assertNotIn(a, b)         | a not in b
+assertIsInstance(a, b)    | isinstance(a, b)
+assertNotIsInstance(a, b) | not isinstance(a, b)
+
+##Test Suite Style##
+```python
+import unittest
+from path_from_root_directory.module_to_be_tested import method_to_be_tested
+    
+class TestRequirement(unittest.TestCase):
+    
+    def setUp(self):
+        # setup objects, variables
+    
+    def tearDown(self):
+        # deallocate / dereference any unused objects
+    
+    def test_module(self):
+        # test cases here
+    
+if __name__ == '__main__':
+    unittest.main()
+```
+##Testing Conventions##
+
+- All **story related**, **public members** must be accompanied by a *complete* testing suite
+- All unit tests should be in the *unit_tests/* subdirectory
