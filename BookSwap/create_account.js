cba99427e32e40dd404284189a1c77e84d1c7de8
brettleighton <brett.leighton@mail.mcgill.ca>
2016-01-30 15:41:41 -0500

    Basic front-end

---
+++ BookSwap/create_account.js
@@ -0,0 +1,3 @@
+function createAccount(){
+	$("#validation").html('<br><div class="alert alert-success" role="alert"><strong>Success!</strong> Your account has been successfully created, and you have been sent an email with an activation link.</div>');
+}
