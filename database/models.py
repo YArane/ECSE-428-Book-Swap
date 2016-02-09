9dd8bdcdab7bcf72c7d01585d169f60dfe35207a
Ruth Berkow <ruth.berkow@mail.mcgill.ca>
2016-02-09 14:56:20 -0500

    Adds date posted field to Post model

--- database/models.py
+++ database/models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask.ext.mongoengine import MongoEngine
 
 db = MongoEngine()
@@ -8,14 +9,17 @@ model below as a table with 3 columns: email, password, and a boolean activated.
 Eventually we will need models for BookPosts, and many others.
 '''
 
+
 class User(db.Document):
     email = db.EmailField(max_length=30, required=True)
     password = db.StringField(max_length=30, required=True)
     activated = db.BooleanField(default=False)
     user_id = db.UUIDField(binary=True, required=True)
 
+
 class Post(db.Document):
     creator = db.ReferenceField('User', max_length=30, required=True)
     textbook_title = db.StringField(max_length=60, required=True)
     textbook_author = db.StringField(max_length=30, required=False)
+    date_posted = db.DateTimeField(default=datetime.datetime.now)
     post_id = db.UUIDField(binary=True, required=True)
