37436a045a32504aeb6056df2c3e2fb974b13e2f
KatieDuff <katherine.duff@mail.mcgill.ca>
2016-03-07 19:32:14 -0500

    Added a unit test for search posts

---
+++ unit_tests/search_posts_tests.py
@@ -0,0 +1,37 @@
+import mongoengine, unittest
+from database.operations import DBOperations
+from database.models import Post
+DB = DBOperations()
+
+
+class SearchPostsTest(unittest.TestCase):
+
+    def setUp(self):
+        mongoengine.connect('testDB', host='localhost')
+        DB.delete_users()
+        DB.insert_user("test@test.com", password="Somepass1234")
+
+    def create_posts(self):
+        creator = DB.get_user_by_email("test@test.com")
+        search_term = "booktitle"
+        post = DB.insert_post(search_term, creator.user_id, "authorname")
+        DB.insert_post("textbook title", creator.user_id, "name of author")
+        search = {'post': post, 'search_term': search_term}
+        return search
+
+    def test_valid_search_posts_existing_title(self):
+        search = self.create_posts()
+        search_post = DB.search(search['search_term'])
+        self.assertEqual(search_post['post'], search_post)
+
+    def search_posts_nonexisting_title(self):
+        search_post = DB.search('nil')
+        self.assertIsNone(search_post)
+
+    def tearDown(self):
+        DB.delete_posts()
+        DB.delete_users()
+        mongoengine.disconnect('testDB', host='localhost')
+
+if __name__ == '__main__':
+    unittest.main()
