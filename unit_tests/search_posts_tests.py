f60c2beaf3c5faf1c98d0ed4c86931c302ae3982
macadev <danielmacario5@gmail.com>
2016-03-09 16:33:24 -0500

    katie fixed her code

--- unit_tests/search_posts_tests.py
+++ unit_tests/search_posts_tests.py
@@ -13,7 +13,8 @@ class SearchPostsTest(unittest.TestCase):
     def create_posts(self):
         creator = DB.get_user_by_email("test@test.com")
         search_term = "booktitle"
-        post = [DB.insert_post(search_term, creator.user_id, "authorname"), DB.insert_post(DB.insert_post(search_term+' extra'))]
+        post = [DB.insert_post(search_term, creator.user_id, "authorname"),
+                DB.insert_post(search_term + ' extra', creator.user_id, "authorname")]
         DB.insert_post("textbook title", creator.user_id, "name of author")
         search = {'post': post, 'search_term': search_term}
         return search
@@ -21,16 +22,17 @@ class SearchPostsTest(unittest.TestCase):
     def test_valid_search_posts_existing_title(self):
         search = self.create_posts()
         search_post = DB.search(search['search_term'])
-        self.assertEqual(search_post['post'], search_post)
+        self.assertEqual(search['post'][0], search_post[0])
+        self.assertEqual(search['post'][1], search_post[1])
 
-    def search_posts_nonexisting_title(self):
+    def test_search_posts_nonexisting_title(self):
         search_post = DB.search('nil')
-        self.assertIsNone(search_post)
+        self.assertEqual(len(search_post), 0)
 
     def tearDown(self):
         DB.delete_posts()
         DB.delete_users()
-        mongoengine.disconnect('testDB', host='localhost')
+        #mongoengine.disconnect('testDB')
 
 if __name__ == '__main__':
     unittest.main()
